103e0533a3ab27f8724408b8e1e74b6ccbf415fe
James Bui <buijvbio@gmail.com>
2018-09-24 11:09:58 -0400

    STYLE: List items will indicate their selection
    via the handleVenueClick method in VenueList.js uses css class 'active'
    to trigger style changes in the selected buttons

--- src/components/VenueList.js
+++ src/components/VenueList.js
@@ -3,17 +3,33 @@ import PropTypes from 'prop-types';
 
 import Search from './Search';
 
+let previousButton; // initialize variable to track the button 
+
 class VenueList extends React.Component {
   static propTypes = {
     locations: PropTypes.array.isRequired // must have passed the venue array
   }
   // note the prop this.prop.handleVenueClick needs to be passed in in order to trigger the infoWindow to be opened
+  
+  
 
   handleVenueClick = (event) => {
     this.props.openInfoWindow(this.props.venueMarkers[event.target.id])
     this.props.changeMarkerIndex(event.target.id); // updates state to reflect selected venue item index
 
+      //simple onclick based css modifications in response to onClick events
+      if (previousButton !== undefined) { // if the previousButton exists
+        previousButton.classList.remove('active'); // toggle the active class on the previous button
+      }
+
+      //note since the ids are also used to as an index value they must be escaped  to be used as css id selectors with '\3', note \ must be escaped as well
+
+      // after further research the CSS.escape(string) method can be used instead
+
+      previousButton = document.querySelector('#'+CSS.escape(event.target.id)); // set previous button to current button
+      previousButton.classList.add('active'); // toggle the active class on the button
   }
+    
 
   
 
